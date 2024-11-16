055b118eb240a2aec3da193aecb89c4300f79b74
Jabin Manandhar <manandhar356@gmail.com>
2024-11-16 16:35:15 +0600

    Created website clone from figma design

---
+++ app.js
@@ -0,0 +1,12 @@
+const countryName = "Country";
+
+const upperCase = countryName.toUpperCase();
+const lowerCase = countryName.toLowerCase();
+
+
+
+// NUMBER METHODS
+
+const randomNumber = "123";
+
+console.log(parseInt(randomNumber,typeof randomNumber));
